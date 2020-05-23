ed64e5d71c4ffa9f93f42674d5273447f17699ae
pengzhe <pengzhepost@sina.com>
2020-05-23 19:18:12 +0800

    LabyrinthProblem.java
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- src/main/java/com/javaman/learning/recursion/LabyrinthProblem.java
+++ src/main/java/com/javaman/learning/recursion/LabyrinthProblem.java
@@ -28,8 +28,8 @@ public class LabyrinthProblem {
         //设挡板 1表示
         map[3][1] = 1;
         map[3][2] = 1;
-        map[1][2] = 1;
-        map[2][2] = 1;
+        // map[1][2] = 1;
+        // map[2][2] = 1;
 
         //输出地图
         for (int i = 0; i < 8; i++) {
@@ -41,7 +41,7 @@ public class LabyrinthProblem {
             System.out.println();
         }
 
-        boolean b = setWay(map, 1, 1);
+        setWay2(map, 1, 1);
         //输出新的地图,小球走过,并标识过的地图
         //输出地图
         log.info("小球走过,并标识的地图");
@@ -69,6 +69,8 @@ public class LabyrinthProblem {
      * 约定:当地图的i,j为0时,表示该点没有走过,当为1时表示墙,不能走,为2时表示是一个通路,可以走
      * 3表示已该位置已探测过,但是走不通
      * 在走迷宫时,需要确定一个策略(方法),先走下面,--》右边---》上---》左,如果该点走不通,在回溯
+     * <p>
+     * 最短路径问题??
      */
     public static boolean setWay(int[][] map, int i, int j) {
         if (map[6][5] == 2) {
@@ -86,6 +88,34 @@ public class LabyrinthProblem {
                 } else if (setWay(map, i - 1, j)) {//向上走
                     return true;
                 } else if (setWay(map, i, j - 1)) {//向左走
+                    return true;
+                } else {
+                    map[i][j] = 3;//说明该点是走不通的,是死路
+                }
+            } else {//如果map[i][j]不等于0;可能是1,2,3
+                return false;
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean setWay2(int[][] map, int i, int j) {
+        if (map[6][5] == 2) {
+            //说明通路已经找到
+            return true;
+        } else {
+            //如果当前这个点还没有走
+            if (map[i][j] == 0) {
+                //按照策略走
+                map[i][j] = 2;//假定该点是可以走通的
+                if (setWay2(map, i - 1, j)) {//向上走
+                    return true;
+                } else if (setWay2(map, i, j + 1)) {//向右走
+                    return true;
+                } else if (setWay2(map, i + 1, j)) {//向下走
+                    return true;
+                } else if (setWay2(map, i, j - 1)) {//向左走
                     return true;
                 } else {
                     map[i][j] = 3;//说明该点是走不通的,是死路
