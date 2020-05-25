25f88f9de1d9dda2c246a00da4571e5c28fc4230
pengzhe <pengzhepost@sina.com>
2020-05-25 23:28:14 +0800

    SeqSearch
    Signed-off-by: pengzhe <pengzhepost@sina.com>

---
+++ src/main/java/com/javaman/learning/search/SeqSearch.java
@@ -0,0 +1,26 @@
+package com.javaman.learning.search;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author pengzhe
+ * @date 2020/5/25 22:30
+ * @description
+ */
+@Slf4j
+public class SeqSearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 9, 11, -1, 34, 89};
+        int i = seqSearch(arr, 34);
+        log.info("i::{}", i);
+    }
+
+    public static int seqSearch(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
